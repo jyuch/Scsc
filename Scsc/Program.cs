150588f57d2fc278e8784ea0eaef73ee1c208563
jyuch <jyuch@users.noreply.github.com>
2019-09-03 20:41:41 +0900

    Compile single num

--- Scsc/Program.cs
+++ Scsc/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,14 @@ namespace Scsc
     {
         static void Main(string[] args)
         {
+            var n = int.Parse(args[0]);
+
             Console.WriteLine(".assembly extern mscorlib { .publickeytoken = (B7 7A 5C 56 19 34 E0 89 ) .ver 4:0:0:0 }");
             Console.WriteLine(".assembly Temp {}");
             Console.WriteLine(".method public static int32 main()");
             Console.WriteLine("{");
             Console.WriteLine(".entrypoint");
-            Console.WriteLine("ldc.i4 42");
+            Console.WriteLine("ldc.i4 {0}", n);
             Console.WriteLine("ret");
             Console.WriteLine("}");
         }
--- Test.ps1
+++ Test.ps1
@@ -2,21 +2,23 @@ $Env:Path += ";C:\Windows\Microsoft.NET\Framework\v4.0.30319";
 
 function Test {
     param (
+        [string] $exp,
         [int] $expected
     )
-    .\Scsc.exe > temp.il
+    .\Scsc.exe $exp > temp.il
     ilasm.exe temp.il /exe /debug=impl /output=temp.exe > $null
     .\temp.exe
 
     if($LASTEXITCODE -eq $expected) {
         Write-Host "OK"
     } else {
-        Write-Host "NG"
+        Write-Host "NG $exp $expected $LASTEXITCODE"
     }
 }
 
 Push-Location .\Scsc\bin\Debug
 
-Test(42)
+Test '42' 42;
+Test '10' 42;
 
 Pop-Location
